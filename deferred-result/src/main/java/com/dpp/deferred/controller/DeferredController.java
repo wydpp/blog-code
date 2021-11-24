9e588de235e71614b7d5f34673f0d333ffc7639c
wydpp <wydupp@163.com>
2021-11-24 22:32:26 +0800

    tomcat线程测试

--- deferred-result/src/main/java/com/dpp/deferred/controller/DeferredController.java
+++ deferred-result/src/main/java/com/dpp/deferred/controller/DeferredController.java
@@ -3,6 +3,7 @@ package com.dpp.deferred.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
@@ -21,40 +22,44 @@ public class DeferredController {
     private ExecutorService executorService = Executors.newSingleThreadExecutor();
 
 
-    @GetMapping("/common")
-    public String commonQuery() throws InterruptedException {
-        logger.info("common开始!");
-        logger.info("common读取文件开始!");
-        TimeUnit.SECONDS.sleep(2);
-        logger.info("common读取文件结束!");
-        logger.info("common结束!");
-        return "common";
+    @GetMapping("/common/{index}")
+    public String commonQuery(@PathVariable("index") String index) throws InterruptedException {
+        logger.info(index + " common开始!");
+        logger.info(index + " common读取文件开始!");
+        TimeUnit.SECONDS.sleep(5);
+        logger.info(index + " common读取文件结束!");
+        logger.info(index + " common结束!");
+        return index + " common";
     }
 
-    @GetMapping("/callable")
-    public Callable<String> callableQuery() {
-        logger.info("callable开始!");
+    @GetMapping("/callable/{index}")
+    public Callable<String> callableQuery(@PathVariable("index") String index) {
+        logger.info(index + " callable开始!");
         Callable<String> callable = () -> {
-            logger.info("callable读取文件开始!");
-            TimeUnit.SECONDS.sleep(2);
-            logger.info("callable读取文件结束!");
-            return "callable";
+            logger.info(index + " callable读取文件开始!");
+            TimeUnit.SECONDS.sleep(5);
+            logger.info(index + " callable读取文件结束!");
+            return index + " callable";
         };
-        logger.info("callable 结束!");
+        logger.info(index + " callable 结束!");
         return callable;
     }
 
-    @GetMapping("/deferred")
-    public DeferredResult<String> deferredQuery() throws InterruptedException {
+    @GetMapping("/deferred/{index}")
+    public DeferredResult<String> deferredQuery(@PathVariable("index") String index) throws InterruptedException {
         logger.info("deferred开始!");
         DeferredResult<String> deferredResult = new DeferredResult<>();
-        TimeUnit.SECONDS.sleep(2);
         executorService.execute(() -> {
-            logger.info("deferred读取文件开始!");
-            deferredResult.setResult("deferred");
-            logger.info("deferred读取文件结束");
+            logger.info(index + " deferred读取文件开始!");
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            deferredResult.setResult(index + " deferred");
+            logger.info(index + " deferred读取文件结束");
         });
-        logger.info("deferred结束!");
+        logger.info(index + " deferred结束!");
         return deferredResult;
     }
 }
