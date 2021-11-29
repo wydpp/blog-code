7ee76138cebb1736164f63811962de73231af79f
duanpengpeng <duanpengpeng@wondertek.com.cn>
2021-11-29 18:12:33 +0800

    deferredResult

--- deferred-result/src/main/java/com/dpp/deferred/controller/DeferredController.java
+++ deferred-result/src/main/java/com/dpp/deferred/controller/DeferredController.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 @RestController
 @RequestMapping("/query")
@@ -33,26 +30,30 @@ public class DeferredController {
     }
 
     @GetMapping("/callable/{index}")
-    public Callable<String> callableQuery(@PathVariable("index") String index) {
+    public String callableQuery(@PathVariable("index") String index) throws Exception {
         logger.info(index + " callable开始!");
-        Callable<String> callable = () -> {
+        Future<String> future = executorService.submit(() -> {
             logger.info(index + " callable读取文件开始!");
-            TimeUnit.SECONDS.sleep(5);
-            logger.info(index + " callable读取文件结束!");
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            logger.info(index + " callable读取文件结束");
             return index + " callable";
-        };
+        });
         logger.info(index + " callable 结束!");
-        return callable;
+        return future.get();
     }
 
     @GetMapping("/deferred/{index}")
     public DeferredResult<String> deferredQuery(@PathVariable("index") String index) throws InterruptedException {
-        logger.info("deferred开始!");
+        logger.info(index + " deferred开始!");
         DeferredResult<String> deferredResult = new DeferredResult<>();
         executorService.execute(() -> {
             logger.info(index + " deferred读取文件开始!");
             try {
-                TimeUnit.SECONDS.sleep(2);
+                TimeUnit.SECONDS.sleep(5);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
