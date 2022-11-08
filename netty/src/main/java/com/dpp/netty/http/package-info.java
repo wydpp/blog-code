ded09e81781a8d6b6c18829db16ce6e242fdd26f
duanpengpeng <duanpengpeng@wondertek.com.cn>
2022-11-08 14:02:19 +0800

    netty构建httpServer

---
+++ netty/src/main/java/com/dpp/netty/http/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName package-info.java
+ * @Author duanpengpeng
+ * @Version 1.0.0
+ * @Description
+ * Netty服务器在6666端口监听，浏览器发出请求"http://localhost:6666/"
+ * 服务器可以回复消息给客户端"Hello!我是服务器"，并对特定请求资源进行过滤
+ * 目的：Netty可以做Http服务开发，理解Handler实例和客户端机器请求的关系
+ * @CreateTime 2022/11/07 15:50:00
+ */
+package com.dpp.netty.http;
